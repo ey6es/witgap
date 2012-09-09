86bcab8ed64106cb95293455ce6be5fd8ba91ba2
andrzej <andrzej@be230a1c-449f-4488-b524-6273046f66f0>
2012-09-09 19:27:30 +0000

    Let* form.
    git-svn-id: file:///export/repos/witgap/trunk@190 be230a1c-449f-4488-b524-6273046f66f0

--- src/scm/Syntax.scm
+++ src/scm/Syntax.scm
@@ -7,3 +7,30 @@
          ((lambda (var ...) expr ...) init ...))
     )
 )
+
+(define-syntax let*
+    (syntax-rules ()
+        ((let* () expr ...)
+         ((lambda () expr ...)))
+        ((let* ((var init)) expr ...)
+         ((lambda (var) expr ...) init))
+        ((let* ((v0 i0) (v1 i1) (vn in) ...) expr ...)
+         ((lambda (v0) (let* ((v1 i1) (vn in) ...) expr ...)) i0))
+    )
+)
+
+(define-syntax and
+    (syntax-rules ()
+        ((and) #t)
+        ((and var) var)
+        ((and var first rest ...) (if var (and first rest ...) #f))
+    )
+)
+
+(define-syntax or
+    (syntax-rules ()
+        ((or) #f)
+        ((or var) var)
+        ((or var first rest ...) (if var #t (or first rest ...)))
+    )
+)
